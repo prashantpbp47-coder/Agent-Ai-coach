bf3603c3c55d33f0ad061e358bf5fd172ff16d65
prashantpbp47-coder <prashant.pbp47@gmail.com>
2026-08-13 01:53:10 +0530

    Register P2 quote bridge

--- foundation/__init__.py
+++ foundation/__init__.py
@@ -2,6 +2,7 @@
 
 from .bootstrap import register_foundation
 from .domain_routes import bp as domain_bp
+from .p2_quote_bridge import bp as quote_bridge_bp
 
 
 def register_p1_domains(app):
@@ -9,4 +10,9 @@ def register_p1_domains(app):
     return app
 
 
-__all__ = ["register_foundation", "register_p1_domains"]
+def register_p2_quote_bridge(app):
+    app.register_blueprint(quote_bridge_bp)
+    return app
+
+
+__all__ = ["register_foundation", "register_p1_domains", "register_p2_quote_bridge"]
