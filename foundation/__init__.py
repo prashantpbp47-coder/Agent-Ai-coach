738dfb94da2510e083fdac7b6786c2a01dd4df93
prashantpbp47-coder <prashant.pbp47@gmail.com>
2026-08-13 02:43:56 +0530

    Register P5 RM planner

--- foundation/__init__.py
+++ foundation/__init__.py
@@ -5,6 +5,7 @@ from .domain_routes import bp as domain_bp
 from .p2_quote_bridge import bp as quote_bridge_bp
 from .p3_provider_routes import bp as provider_bp
 from .rm_routes import bp as rm_bp
+from .p5_routes import bp as p5_bp
 
 
 def register_p1_domains(app):
@@ -27,10 +28,16 @@ def register_p4_rm_command_center(app):
     return app
 
 
+def register_p5_rm_planner(app):
+    app.register_blueprint(p5_bp)
+    return app
+
+
 __all__ = [
     "register_foundation",
     "register_p1_domains",
     "register_p2_quote_bridge",
     "register_p3_providers",
     "register_p4_rm_command_center",
+    "register_p5_rm_planner",
 ]
